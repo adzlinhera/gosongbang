61f37df2aa7d6333a7d28208b37000b74190b38e
adzlinhera <adzlinherawati@gmail.com>
2025-03-21 06:02:53 +0700

    Create tari.js

---
+++ js/tari.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'e82bdb8f966bddde3b128743cffde9ca',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/e82bdb8f966bddde3b128743cffde9ca/invoke.js"></scr' + 'ipt>');
